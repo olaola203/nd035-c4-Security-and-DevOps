6973ecb07327b7265809f3258a086e99730ee8f1
Kevin Vo <daivo203@gmail.com>
2024-10-16 13:41:53 +0700

    add authen, author and logger

--- src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -4,14 +4,35 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class CreateUserRequest {
 
-	@JsonProperty
-	private String username;
+    @JsonProperty
+    private String username;
+    @JsonProperty
+    private String password;
 
-	public String getUsername() {
-		return username;
-	}
+    @JsonProperty
+    private String confirmPassword;
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
 }
